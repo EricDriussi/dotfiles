e2a725117c242ca32931f30bb883e539b11ff76f
Eric Driussi <ericdriussi@gmail.com>
2022-02-24 07:43:51 +0000

    General Updates

--- base/.custom-zsh/my-zsh.zsh
+++ base/.custom-zsh/my-zsh.zsh
@@ -11,3 +11,14 @@ source ~/.custom-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.custom-zsh/plugins/git.zsh
 source ~/.custom-zsh/plugins/nvm.zsh
 
+# Show info in bar
+if [[ "${TERM}" != "" && "${TERM}" == "alacritty" ]] then
+    precmd(){
+        # output CWD
+        print -Pn "\e]0;Alacritty - %~\a"
+    }
+    preexec(){
+        # output executed command
+        echo -en "\e]0;Alacritty - ${1}\a"
+    }
+fi
