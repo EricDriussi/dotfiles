f6d6e6a4a862efbf5cba9e08702224273b398397
Eric Driussi <ericdriussi@gmail.com>
2022-08-01 20:14:12 +0100

    format and add to zsh bindings

--- custom-zsh/.config/custom-zsh/bindings.zsh
+++ custom-zsh/.config/custom-zsh/bindings.zsh
@@ -1,43 +1,43 @@
 # Make sure that the terminal is in application mode when zle is active, since
 # only then values from $terminfo are valid
 if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-  function zle-line-init() {
-    echoti smkx
-  }
-  function zle-line-finish() {
-    echoti rmkx
-  }
-  zle -N zle-line-init
-  zle -N zle-line-finish
+    function zle-line-init() {
+        echoti smkx
+    }
+    function zle-line-finish() {
+        echoti rmkx
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
 fi
 
 # Start typing + [Up-Arrow] - fuzzy find history forward
 if [[ -n "${terminfo[kcuu1]}" ]]; then
-  autoload -U up-line-or-beginning-search
-  zle -N up-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
+    autoload -U up-line-or-beginning-search
+    zle -N up-line-or-beginning-search
+    bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
 fi
 
 # Start typing + [Down-Arrow] - fuzzy find history backward
 if [[ -n "${terminfo[kcud1]}" ]]; then
-  autoload -U down-line-or-beginning-search
-  zle -N down-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
+    autoload -U down-line-or-beginning-search
+    zle -N down-line-or-beginning-search
+    bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
 # [Home] - Go to beginning of line
 if [[ -n "${terminfo[khome]}" ]]; then
-  bindkey -M viins "${terminfo[khome]}" beginning-of-line
+    bindkey -M viins "${terminfo[khome]}" beginning-of-line
 fi
 
 # [End] - Go to end of line
 if [[ -n "${terminfo[kend]}" ]]; then
-  bindkey -M viins "${terminfo[kend]}"  end-of-line
+    bindkey -M viins "${terminfo[kend]}"  end-of-line
 fi
 
 # [Shift-Tab] - move through the completion menu backwards
 if [[ -n "${terminfo[kcbt]}" ]]; then
-  bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
+    bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
 # [Ctrl-RightArrow] - move forward one word
@@ -59,3 +59,12 @@ bindkey '^[[3;5~' kill-word
 autoload -U edit-command-line
 bindkey '\033' edit-command-line
 zle -N edit-command-line
+
+# Vi mode
+# bindkey -v
+
+# Vim keys in auto-complete menu
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
