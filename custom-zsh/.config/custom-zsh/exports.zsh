85d3390bdbdbf72d1ca6c308e93dab6202e8547f
Eric Driussi <ericdriussi@gmail.com>
2022-08-01 20:13:24 +0100

    fixing exports

--- custom-zsh/.config/custom-zsh/exports.zsh
+++ custom-zsh/.config/custom-zsh/exports.zsh
@@ -1,8 +1,11 @@
 export TERMINAL="kitty"
 export EDITOR="nvim"
 export VISUAL="nvim"
-export ZSH="~/.custom-zsh"
+export ZSH="$HOME/.custom-zsh"
 export MANPAGER='nvim +Man!'
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$XDG_CONFIG_HOME/local/share"
+export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
 
 export PATH=$HOME/bin:/usr/bin:$PATH
 export PATH=$PATH:/opt
@@ -12,8 +15,8 @@ export PATH="/home/eric/.deno/bin:$PATH"
 export PATH=$PATH:/usr/local/nodejs/bin
 export NVM_DIR="$HOME/.nvm"
 export NVM_LAZY_LOAD=true
- #nvm won't persist otherwhise ¯\_(ツ)_/¯
 #[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+# nvm won't persist otherwhise ¯\_(ツ)_/¯
 # yes node, please take all my RAM
 export NODE_OPTIONS=--max_old_space_size=6000
 
@@ -28,11 +31,10 @@ export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules/" -g "!.git/" --hidden 
 export FZF_DEFAULT_OPTS='--height 70% --layout=reverse --border --preview="head -$LINES {}" --info=inline'
 
 # zsh_history settings
-HISTFILE=~/.zsh_history
+export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=10000
 export SAVEHIST=10000
 setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
 
-# Vi mode
-#bindkey -v
 export KEYTIMEOUT=1
