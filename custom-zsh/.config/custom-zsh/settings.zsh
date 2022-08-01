d674e2a4dfba9c9a7dbc544fc3ff3534f94b3727
Eric Driussi <ericdriussi@gmail.com>
2022-08-01 20:13:40 +0100

    sort and comment settings

--- custom-zsh/.config/custom-zsh/settings.zsh
+++ custom-zsh/.config/custom-zsh/settings.zsh
@@ -1,16 +1,22 @@
-# Enable colors and change prompt:
-autoload -U colors && colors
-# Black magick to make stuff work sensibly w/o OMZ
-autoload -U compinit && compinit -u
-zstyle ':completion:*' menu select
-# Auto complete with case insenstivity
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# Enable auto-completion
 zmodload zsh/complist
-compinit
+autoload -U compinit && compinit -u
+# Auto-complete hidden files
+_comp_options+=(globdots)
+# Case insensitive auto-complete
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# Use completion menu
+zstyle ':completion:*' menu select
+# Formatting completion
+zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
+
+# Disables error if autocompletion returns no match
 unsetopt nomatch
+# Enable colors
+autoload -U colors && colors
 
 # Show info in polybar
-if [[ "${TERM}" != "" && "${TERM}" == "alacritty" ]] then
+if [[ "${TERM}" != "" && "${TERM}" == "alacritty" || "${TERM}" == "kitty" ]] then
     precmd(){
         # output CWD
         print -Pn "\e]0;$USER 💀 %~\a"
