a99b64fe94507a5afdf2f9149f217f1dcf0a2669
Eric Driussi <ericdriussi@gmail.com>
2022-08-01 09:19:03 +0100

    sort aliases

--- custom-zsh/.config/custom-zsh/aliases.zsh
+++ custom-zsh/.config/custom-zsh/aliases.zsh
@@ -1,91 +1,90 @@
+# Sys
+alias cl="clear"
+alias diskUsage="sudo du -h | sort -hr | head -10"
+alias grep="grep --color=always"
 alias l="exa -bghla"
 alias mkdir="mkdir -p"
-alias tre="tree -L 2 -C -a -I 'node_modules' -I 'build' -I '.git' -I '.idea'"
-alias sctl="sudo systemctl"
-alias grep="grep --color=always"
 alias rmd="rm -rf"
-alias cpd="cp -r"
-alias v="nvim"
-alias basev="nvim -u ~/.config/nvim/base-init.vim"
-alias cb="cd .."
-alias cc="z"
-alias cl="clear"
-
-mkd() { mkdir "$1" && cd "$1" }
-c() { cd "$1" && l }
-vo() { nvim "$(fzf)" }
-lo() { lvim "$(fzf)" }
-ports() { sudo ss -tulpn | grep LISTEN }
+alias sctl="sudo systemctl"
+alias tre="tree -L 2 -C -a -I 'node_modules' -I 'build' -I '.git' -I '.idea'"
 fd() { find . -iname "*"$1"*" | sort }
-alias qrpaste="xclip -o | qrencode -t utf8"
-alias bt='btop'
 
-alias vfm=~/.config/vifm/vifmimg/vifmrun
-alias vf='vfm .'
 # Change ssh key
 alias ssauth='eval "$(ssh-agent -s)" && ssh-add'
 
+# Navigation
+alias cb="cd .."
+alias cc="z"
+alias cpd="cp -r"
+c() { cd "$1" && l }
+mkd() { mkdir "$1" && cd "$1" }
+ports() { sudo ss -tulpn | grep LISTEN }
+
+# Misc
 alias blogUpdate="cd ~/Documents/projects/website/ && hugo -D && rsync -rtvzP --rsh=ssh ~/Documents/projects/website/public/* ansible@unixmagick.xyz:/var/www/website"
+alias bt='btop'
+alias qrpaste="xclip -o | qrencode -t utf8"
+alias vf='vfm .'
+alias vfm=~/.config/vifm/vifmimg/vifmrun
+
+# Nvim
+alias basev="nvim -u ~/.config/nvim/base-init.vim"
+alias v="nvim"
+alias voa="nvim ~/dotfiles/custom-zsh/.config/custom-zsh/aliases.zsh"
+alias voi="nvim ~/dotfiles/i3/.config/i3/config"
+alias vov="cd ~/.config/nvim/ && nvim init.vim"
+alias voz="nvim ~/dotfiles/custom-zsh/.config/custom-zsh/exports.zsh"
+vo() { nvim "$(fzf)" }
+
+# Pacman
+alias cleansys="rm -rf ~/.cache/paru; yes | sudo paccache -rk2 -ruk0; yes | sudo pacman -Sc; yes | paru -Sc --aur"
+alias install="sudo pacman -S"
+alias remove="sudo pacman -Rs"
+alias search="sudo pacman -Ss"
+alias updatesys="sudo pacman -Syu"
+
+# Dev
 alias nr='npm run'
 alias nv='source /usr/share/nvm/init-nvm.sh && nvm'
 alias pr='pipenv run'
 
-# Shortcuts
-alias vov="cd ~/.config/nvim/ && nvim init.vim"
-alias voz="nvim ~/dotfiles/custom-zsh/.config/custom-zsh/exports.zsh"
-alias voa="nvim ~/dotfiles/custom-zsh/.config/custom-zsh/aliases.zsh"
-alias voi="nvim ~/dotfiles/i3/.config/i3/config"
-
-# Pacman
-alias updatesys="sudo pacman -Syu"
-alias install="sudo pacman -S"
-alias remove="sudo pacman -Rs"
-alias search="sudo pacman -Ss"
-alias cleansys="rm -rf ~/.cache/paru; yes | sudo paccache -rk2 -ruk0; yes | sudo pacman -Sc; yes | paru -Sc --aur"
-alias diskUsage="sudo du -h | sort -hr | head -10"
-
-coa (){ python3 -B ~/.co-author.py $1 $2 }
 # Git
+coa (){ python3 -B ~/.co-author.py $1 $2 }
+gaddorigin() { git remote add origin "$1"; git remote set-url --add --push origin "$1" }
+gmkb() { git branch -b "$1" && git push --set-upstream origin "$1" }
+trymerge() { git merge "$1" --no-commit --no-ff; git merge --abort }
 alias lg='lazygit'
+alias gp='git pull --autostash'
 alias gs='git status'
 alias gadd='git add'
-alias gunstage='git restore --staged .'
 alias gc='git commit'
 alias gcom='git add -A && git commit'
+alias gpush='git push'
+alias gmv='git checkout'
+alias rollback='git restore'
+alias gunstage='git restore --staged .'
 alias gstash='git stash -u'
 alias gunstash='git stash pop'
 alias gamend='git add --all && git commit --amend'
-alias gp='git pull --autostash'
 alias gsuperp='git fetch origin && git reset --hard origin'
-alias gpush='git push'
-alias gmv='git checkout'
+alias ginit='git init && git config credential.helper store'
+alias gaddremote='git remote set-url --add --push origin'
 alias gme='git merge --no-squash'
 alias gres='git mergetool'
-trymerge() { git merge "$1" --no-commit --no-ff; git merge --abort }
-alias rollback='git restore'
 alias gab='git branch -a'
 alias grmb='git branch -D'
-gmkb() { git branch -b "$1" && git push --set-upstream origin "$1" }
-alias ginit='git init && git config credential.helper store'
-gaddorigin() { git remote add origin "$1"; git remote set-url --add --push origin "$1" }
-alias gaddremote='git remote set-url --add --push origin'
 alias glg='git log -15 --graph --abbrev-commit --decorate --format=tformat:"%C(yellow)%h%C(reset)%C(reset)%C(auto)%d%C(reset) %s %C(white) -  %C(bold green)(%ar)%C(reset) %C(dim blue)<%an>%Creset"'
 alias glog=' git log -10  --name-only --graph --abbrev-commit --decorate --format=tformat:"%C(yellow)%h%C(reset)%C(reset)%C(auto)%d%C(reset) %s %C(white) -  %C(bold green)(%ar)%C(reset) %C(dim blue)<%an>%C(reset)" '
 
 # Docker
-alias dnuke='docker stop $(docker container ls -a -q); docker system prune -a -f --volumes'
-alias dup='docker-compose up -d'
-alias ddown='docker-compose down'
-alias dstop='docker stop'
-alias di='docker image ls'
 alias dc='docker container ls --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}"'
+alias ddown='docker-compose down'
+alias di='docker image ls'
+alias dnuke='docker stop $(docker container ls -a -q); docker system prune -a -f --volumes'
+alias dstop='docker stop'
+alias dup='docker-compose up -d'
 alias dv='docker volume ls'
 alias dlist='echo -e "------------------------------------IMAGES------------------------------------" && di && echo -e "----------------------------------CONTAINERS-----------------------------------" && dc && echo -e "------------------------------------VOLUMES------------------------------------" && dv'
-
-# youtube-dl
-dlvideo() { noglob yt-dlp -o "%(title)s.%(ext)s" -P "~/Videos/" "$1" }
-dlalbum() { noglob yt-dlp --audio-format best -x --audio-quality 0 -o "%(album)s/%(title)s.%(ext)s" "$1" }
-dlsong() { noglob yt-dlp --audio-format best -x --audio-quality 0 -o "%(title)s.%(ext)s" "$1" }
 
 # Show ctrl+c
 copyToStdout() {
