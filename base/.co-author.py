d7fe20259955f1a9cf3af9d21b001026f38ad3fe
Eric Driussi <ericdriussi@gmail.com>
2022-08-05 12:36:48 +0100

    add printing to coa script

--- base/.co-author.py
+++ base/.co-author.py
@@ -6,19 +6,44 @@ from authorsMap import authors
 
 def buildCommit(message: str, commaSeparatedAliases: str):
     authors = mapToAvailableAuthors(commaSeparatedAliases.split(","))
+    formatted_message = f"{message}\n\n{authors}"
 
-    command = 'git commit -m "%s%s%s"' % (message, "\n\n", authors)
+    command = ["git", "commit", "-m", formatted_message]
 
-    subprocess.run(command, shell=True)
+    subprocess.run(command)
 
 
-def mapToAvailableAuthors(authors2):
-    return "\n".join(authors[author] for author in authors2)
+def mapToAvailableAuthors(aliases: list) -> str:
+    return "\n".join(authors[alias] for alias in aliases)
+
+
+def get_full_name_from(item: tuple) -> str:
+    return item[1].split(':')[1].split('<')[0].strip()
+
+
+def get_author_string_from(item: tuple) -> str:
+    return "⦔ " + item[0] + " -> " + get_full_name_from(item)
+
+
+def print_authors(authors: dict):
+    for index, item in enumerate(authors.items()):
+        half = int(len(authors) / 2)
+        if index >= half:
+            break
+
+        other_item = list(authors.items())[half + index]
+
+        print(get_author_string_from(item) + "\t\t" +
+              get_author_string_from(other_item) + "\n")
 
 
 if __name__ == "__main__":
     try:
-        buildCommit(message=sys.argv[2], commaSeparatedAliases=sys.argv[1])
+        print_authors(authors)
+        co_authors = input(
+            'Enter your coworkers initials separated by commas: \n')
+        message = input('Enter your commit message: \n')
+        buildCommit(message, co_authors)
     except IndexError:
         print("INVALID PARAMETERS")
         sys.exit(1)
