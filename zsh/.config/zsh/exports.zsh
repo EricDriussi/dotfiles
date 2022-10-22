44a20ab969a53e977680ef39df808af1a660a288
Eric Driussi <ericdriussi@gmail.com>
2022-10-22 10:14:47 +0100

    improve fzf style

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -22,5 +22,8 @@ export PATH=$PATH:$HOME/.config/vifm/vifmimg
 
 # fzf defaults
 export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules/" -g "!.git/" --hidden .'
-export FZF_DEFAULT_OPTS='--height 70% --layout=reverse --border --preview="head -$LINES {}" --info=inline'
-
+export FZF_DEFAULT_OPTS=" \
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+    --height 70% --layout=reverse --border --preview=\"head -$LINES {}\" --info=inline"
