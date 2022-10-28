94da7afe72cf1886a8ce98dfba524283ac5c6783
Eric Driussi <ericdriussi@gmail.com>
2022-10-28 16:02:07 +0100

    update exports

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,24 +3,26 @@ export EDITOR="nvim"
 export VISUAL="nvim"
 export MANPAGER='nvim +Man!'
 
-export PATH=$HOME/bin:/usr/bin:$PATH
+# PATH
+export PATH=$PATH:/usr/bin
 export PATH=$PATH:/opt
-
-# Nodejs
-export PATH=$HOME/.deno/bin:$PATH
-export PATH=$PATH:/usr/local/nodejs/bin
-export NVM_DIR="$HOME/.nvm"
-export NVM_LAZY_LOAD=true
-# yes node, please take all my RAM
-export NODE_OPTIONS=--max_old_space_size=6000
-
+# nvim - mason
+export PATH=$PATH:/$HOME/.config/local/share/nvim/mason/bin
+# intelliJ
 export PATH=$PATH:$HOME/intellijUlt/bin
-
 # vifm
 export PATH=$PATH:$HOME/.config/vifm
 export PATH=$PATH:$HOME/.config/vifm/vifmimg
+# nodejs
+export PATH=$PATH:$HOME/.deno/bin
+export PATH=$PATH:/usr/local/nodejs/bin
 
-# fzf defaults
+# NVM - NODE
+export NVM_DIR=$HOME/.nvm
+export NVM_LAZY_LOAD=true
+export NODE_OPTIONS=--max_old_space_size=6000
+
+# FZF
 export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules/" -g "!.git/" --hidden .'
 export FZF_DEFAULT_OPTS=" \
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
