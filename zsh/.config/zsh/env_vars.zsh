77c873a76acd0a228b351a6ef3cb6c1955e24e18
Eric Driussi <ericdriussi@gmail.com>
2022-11-23 07:42:38 +0000

    fzf col

--- zsh/.config/zsh/env_vars.zsh
+++ zsh/.config/zsh/env_vars.zsh
@@ -25,7 +25,4 @@ export NODE_OPTIONS=--max_old_space_size=6000
 # FZF
 export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules/" -g "!.git/" --hidden .'
 export FZF_DEFAULT_OPTS=" \
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
     --height 70% --layout=reverse --border --preview=\"head -$LINES {}\" --info=inline"
