d6103ce96beb926c802b0fb06555ddad087fe9f7
Eric Driussi <ericdriussi@gmail.com>
2022-12-16 15:17:10 +0000

    picom

--- wezterm/.config/wezterm/gruv.lua
+++ wezterm/.config/wezterm/gruv.lua
@@ -1,6 +1,7 @@
 return {
-  name = "gruvbox_material_dark_hard",
-  scheme = {
+  hard = "gruvbox_material_dark_hard",
+  medium = "gruvbox_material_dark_medium",
+  schemes = {
     ["gruvbox_material_dark_hard"] = {
 
       foreground = "#D4BE98",
@@ -13,6 +14,18 @@ return {
 
       ansi = { "#1d2021", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
       brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+    },
+    ["gruvbox_material_dark_medium"] = {
+      foreground = "#dfbf8e",
+      background = "#282828",
+      cursor_bg = "#dfbf8e",
+      cursor_border = "#dfbf8e",
+      cursor_fg = "#282828",
+      selection_bg = "#dfbf8e",
+      selection_fg = "#45403d",
+
+      ansi = { "#282828", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#dfbf8e" },
+      brights = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#dfbf8e" },
     },
   },
 }
