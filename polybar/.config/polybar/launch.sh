01aee78973d0451a7fb26a702ecd6ad17d22b983
Eric Driussi <ericdriussi@gmail.com>
2023-01-15 14:10:19 +0000

    improve polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar HDMI &
-polybar eDP1 &
+# Launch based on available outputs
+outputs=$(xrandr --query | grep " connected")
+if [[ $outputs == *"HDMI"* ]]; then
+	polybar Main &
+	polybar Secondary &
+else
+	polybar Single &
+fi
