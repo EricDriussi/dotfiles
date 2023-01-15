0d2df20054c5d11d109bd01a55a9805856c7d7fc
Eric Driussi <ericdriussi@gmail.com>
2023-01-15 14:09:45 +0000

    improve monitor layout

--- screenlayout/.config/screenlayout/monitor.sh
+++ screenlayout/.config/screenlayout/monitor.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-xrandr --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal
+#!/bin/bash
+
+outputs=$(xrandr --query | grep " connected")
+
+if [[ $outputs == *"HDMI"* ]]; then
+	xrandr --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal
+else
+	xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+fi
