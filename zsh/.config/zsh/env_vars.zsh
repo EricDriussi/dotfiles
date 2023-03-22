da42e2666e5f5ad13ea54dd7ef64dccd3f7c389d
Eric Driussi <ericdriussi@gmail.com>
2023-03-22 07:46:03 +0000

    cleanup vars

--- zsh/.config/zsh/env_vars.zsh
+++ zsh/.config/zsh/env_vars.zsh
@@ -1,19 +1,18 @@
-export TERMINAL="wezterm"
-export EDITOR="nvim"
-export VISUAL="nvim"
+export TERMINAL=wezterm
+export EDITOR=nvim
+export VISUAL=nvim
 export MANPAGER='nvim +Man!'
 
 # PATH
 export PATH=$PATH:/usr/bin
 export PATH=$PATH:/opt
-export PATH=/opt/homebrew/bin:$PATH
-export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH=$PATH:/opt/homebrew/bin
+export PATH=$PATH:/opt/homebrew/opt/gnu-sed/libexec/gnubin
 # nvim - mason
 export PATH=$PATH:/$HOME/.config/local/share/nvim/mason/bin
 # intelliJ
 export PATH=$PATH:$HOME/intellijUlt/bin
 # vifm
-export PATH=$PATH:$HOME/.config/vifm
 export PATH=$PATH:$HOME/.config/vifm/vifmimg
 # nodejs
 export PATH=$PATH:$HOME/.deno/bin
@@ -22,7 +21,7 @@ export PATH=$PATH:/usr/local/nodejs/bin
 # NVM - NODE
 export NVM_DIR=$HOME/.nvm
 export NVM_LAZY_LOAD=true
-export NODE_OPTIONS=--max_old_space_size=6000
+export NODE_OPTIONS='--max_old_space_size=6000'
 
 # XDG
 export XDG_DATA_HOME=$XDG_CONFIG_HOME/local/share
@@ -31,6 +30,5 @@ export XDG_CACHE_HOME=$XDG_CONFIG_HOME/cache
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=240'
 
 # FZF
-export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules/" -g "!.git/" --hidden .'
-export FZF_DEFAULT_OPTS=" \
-    --height 70% --layout=reverse --border --preview=\"head -$LINES {}\" --info=inline"
+export FZF_DEFAULT_COMMAND='rg --files -g "!node_modules" -g "!.git" --hidden .'
+export FZF_DEFAULT_OPTS='--height 70% --layout=reverse --border --preview="head -$LINES {}" --info=inline'
