8ac1951709ac00e77bd5aa5d186a0adb825c8dfe
Eric Driussi <ericdriussi@gmail.com>
2023-09-14 10:04:13 +0100

    exa -> eza

--- zsh/.config/zsh/aliases/misc.zsh
+++ zsh/.config/zsh/aliases/misc.zsh
@@ -4,7 +4,7 @@ alias cc="z"
 alias cl="clear"
 alias cpd="cp -ir"
 alias grep="grep --color=always"
-alias l="exa --group --all --long --icons"
+alias l="eza --group --all --long --icons"
 alias c=cd_and_ls; cd_and_ls() { cd "$1" && l }
 alias mkd=mkdir_and_cd; mkdir_and_cd() { mkdir -p "$1" && cd "$1" }
 alias mkdir="mkdir -p"
@@ -13,7 +13,7 @@ alias rg="rg --hidden -g '!.git'"
 alias rmd="rm -rf"
 alias sctl="sudo systemctl"
 alias src="source $ZDOTDIR/.zshrc"
-alias tre="exa --all --icons --group-directories-first --tree --level=2 --ignore-glob='node_modules|.git|.idea'"
+alias tre="eza --all --icons --group-directories-first --tree --level=2 --ignore-glob='node_modules|.git|.idea'"
 alias tree="tree -L 2 -C -a -I 'node_modules' -I 'build' -I '.git' -I '.idea'"
 alias vf="vfm ."
 alias vfm=~/.config/vifm/vifmimg/vifmrun
