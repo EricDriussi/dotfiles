ffcedb6b76bad14379e835d1f80c3fb2df3fdd09
Eric Driussi <ericdriussi@gmail.com>
2023-12-27 16:49:56 +0000

    improve screenlayout script

--- screenlayout/.config/screenlayout/monitor.sh
+++ screenlayout/.config/screenlayout/monitor.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
+set -o nounset -o errexit -o pipefail
+[[ "${TRACE-0}" == "1" ]] && set -o xtrace
 
-xrandr_output=$(xrandr)
-monitors=$(echo "$xrandr_output" | \grep -i " connected" | cut -d" " -f1)
+main() {
+	local laptop_monitor
+	local hdmi_monitor
 
-edp=$(echo "$monitors" | \grep -i "edp")
-edp_max_res=$(echo "$xrandr_output" | \grep -i "$edp" -A 1 | \grep -v " connected" | awk '{print $1}')
+	xrandr_output=$(xrandr)
+	monitors=$(echo "$xrandr_output" | \grep -i " connected" | awk '{print $1}')
+	laptop_monitor=$(echo "$monitors" | \grep -i "edp")
+	hdmi_monitor=$(echo "$monitors" | \grep -i "hdmi")
 
-hdmi=$(xrandr --listactivemonitors | cut -d" " -f6 | \grep -i "hdmi")
-if [[ $hdmi ]]; then
-	setup_laptop_plus_ext_monitor
-else
-	setup_only_laptop
-fi
-
-function setup_only_laptop {
-	xrandr --output "$edp" --primary --mode "$edp_max_res" --pos 0x0 --rotate normal
-}
-
-function setup_laptop_plus_ext_monitor {
-	hdmi_max_res=$(echo "$xrandr_output" | \grep -i "$hdmi" -A 1 | \grep -v " connected" | awk '{print $1}')
-	laptop_pos="0x360"
-	ext_monitor_pos="1920x0"
-	if [[ -f "/etc/arch-release" ]]; then
-		laptop_pos="2560x360"
-		ext_monitor_pos="0x0"
+	if [[ $hdmi_monitor ]]; then
+		xrandr_args=$(setup_laptop_plus_ext_monitor "$xrandr_output" "$laptop_monitor" "$hdmi_monitor")
+		eval "xrandr $xrandr_args"
+		exit
 	fi
-	xrandr --output "$hdmi" --mode "$hdmi_max_res" --pos "$ext_monitor_pos" --rotate normal --output "$edp" --primary --mode "$edp_max_res" --pos "$laptop_pos" --rotate normal
+
+	xrandr_args=$(setup_only_laptop "$xrandr_output" "$laptop_monitor")
+	eval "xrandr $xrandr_args"
+	exit
 }
+
+setup_only_laptop() {
+	local xrandr_out="$1"
+	local laptop_monitor="$2"
+	local laptop_max_res
+
+	laptop_max_res=$(echo "$xrandr_out" | \grep -i "$laptop_monitor" -A 1 | \grep -v " connected" | awk '{print $1}')
+
+	echo "--output \"$laptop_monitor\" --primary --mode \"$laptop_max_res\" --pos 0x0 --rotate normal"
+}
+
+setup_laptop_plus_ext_monitor() {
+	local xrandr_out="$1"
+	local laptop_monitor="$2"
+	local hdmi_monitor="$3"
+	local laptop_max_res
+	local hdmi_max_res
+
+	laptop_max_res=$(echo "$xrandr_out" | \grep -i "$laptop_monitor" -A 1 | \grep -v " connected" | awk '{print $1}')
+	hdmi_max_res=$(echo "$xrandr_output" | \grep -i "$hdmi_monitor" -A 1 | \grep -v " connected" | awk '{print $1}')
+
+	echo "--output \"$hdmi_monitor\" --mode \"$hdmi_max_res\" --pos \"$(get_ext_monitor_position)\" --rotate normal --output \"$laptop_monitor\" --primary --mode \"$laptop_max_res\" --pos \"$(get_laptop_position)\" --rotate normal"
+}
+
+get_laptop_position() {
+	if [[ -f "/etc/arch-release" ]]; then
+		echo "2560x360"
+	fi
+	echo "0x360"
+}
+get_ext_monitor_position() {
+	if [[ -f "/etc/arch-release" ]]; then
+		echo "0x0"
+	fi
+	echo "1920x0"
+}
+
+main
