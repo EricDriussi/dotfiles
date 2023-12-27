c8d4a854a18b218b62904ec8d3bfb12c15b3d780
Eric Driussi <ericdriussi@gmail.com>
2023-12-27 09:59:11 +0000

    separate config for work laptop

--- screenlayout/.config/screenlayout/monitor.sh
+++ screenlayout/.config/screenlayout/monitor.sh
@@ -8,8 +8,22 @@ edp_max_res=$(echo "$xrandr_output" | \grep -i "$edp" -A 1 | \grep -v " connecte
 
 hdmi=$(xrandr --listactivemonitors | cut -d" " -f6 | \grep -i "hdmi")
 if [[ $hdmi ]]; then
-	hdmi_max_res=$(echo "$xrandr_output" | \grep -i "$hdmi" -A 1 | \grep -v " connected" | awk '{print $1}')
-	xrandr --output "$hdmi" --mode "$hdmi_max_res" --pos 0x0 --rotate normal --output "$edp" --primary --mode "$edp_max_res" --pos 2560x360 --rotate normal
+	setup_laptop_plus_ext_monitor
 else
-	xrandr --output "$edp" --primary --mode "$edp_max_res" --pos 0x0 --rotate normal
+	setup_only_laptop
 fi
+
+function setup_only_laptop {
+	xrandr --output "$edp" --primary --mode "$edp_max_res" --pos 0x0 --rotate normal
+}
+
+function setup_laptop_plus_ext_monitor {
+	hdmi_max_res=$(echo "$xrandr_output" | \grep -i "$hdmi" -A 1 | \grep -v " connected" | awk '{print $1}')
+	laptop_pos="0x360"
+	ext_monitor_pos="1920x0"
+	if [[ -f "/etc/arch-release" ]]; then
+		laptop_pos="2560x360"
+		ext_monitor_pos="0x0"
+	fi
+	xrandr --output "$hdmi" --mode "$hdmi_max_res" --pos "$ext_monitor_pos" --rotate normal --output "$edp" --primary --mode "$edp_max_res" --pos "$laptop_pos" --rotate normal
+}
