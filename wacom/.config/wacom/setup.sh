cbd9ff44156662e0688398c12d1dd18d6f9423a5
Eric Driussi <ericdriussi@gmail.com>
2024-07-25 20:53:55 +0100

    add wacom bindings

--- wacom/.config/wacom/setup.sh
+++ wacom/.config/wacom/setup.sh
@@ -6,3 +6,7 @@ IDs=$(xinput | grep "Wacom" | cut -f 2 | cut -c 4-5)
 echo "$IDs" | while IFS= read -r ID; do
 	xinput map-to-output "$ID" "$main_monitor"
 done
+
+wacom_tablet="Wacom Intuos BT M Pad pad"
+xsetwacom set "$wacom_tablet" button 3 'key +ctrl z -ctrl'
+xsetwacom set "$wacom_tablet" button 8 'key +ctrl +shift z -ctrl -shift'
